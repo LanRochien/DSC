0fa33c4fda68e9e7c7b0fe5b1f09bed6e391eb9c
lanke-people <1342895831@qq.com>
2023-06-28 12:07:08 +0800

    post自己的更新

--- server_side/src/main/java/com/cupk/mapper/PostMapper.java
+++ server_side/src/main/java/com/cupk/mapper/PostMapper.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface PostMapper {
     List<Post> getMainPostsByPlateID(Integer plate_id);//通过版号查询所有主题帖
     int insertMainPost(Post post);//插入主题帖
+    List<Post> findMainPostsByStr(String Str);//论坛的模糊搜索
 }
--- server_side/src/main/java/com/cupk/service/PostService.java
+++ server_side/src/main/java/com/cupk/service/PostService.java
@@ -9,4 +9,5 @@ public interface PostService {
 
     List<Post> getMainPostsByPlateID(Integer plate_id);//通过版号查询所有主题帖
     int insertMainPost(Post post);//插入主题帖
+    List<Post> findMainPostsByStr(String Str);//论坛的模糊搜索
 }
