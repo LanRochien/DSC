976d788beeaac7fb0567e5843ed2089b9326016d
lanke-people <1342895831@qq.com>
2023-06-29 12:03:33 +0800

    comment更新

--- server_side/src/main/java/com/cupk/service/CommentService.java
+++ server_side/src/main/java/com/cupk/service/CommentService.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface CommentService {
     List<Comment> findCommentsByPostID(Integer post_id);//通过posIDt查找comments
+    int insertComment(Comment comment);//提交comment
 }
